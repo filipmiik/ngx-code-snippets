53893d9480b373371da84bf9b58805d1cb443df9
Filip Mík <filip@miik.cz>
2020-11-04 20:05:22 +0100

    Fix null error in route-fragment-smooth-scroll.ts

--- route-fragment-smooth-scroll.ts
+++ route-fragment-smooth-scroll.ts
@@ -61,23 +61,25 @@ export class AppComponent implements AfterViewInit, OnDestroy {
         takeUntil(this.ngUnsubscribe)
       )
       .subscribe(frag => {
-        const id = '#' + frag.trim();
+        if (typeof frag === 'string') {
+          const id = '#' + frag.trim();
+          
+          if (id.length > 1) {
+            const fragElement = document.getElementById(frag);
 
-        if (typeof frag === 'string' && id.length > 1) {
-          const fragElement = document.getElementById(frag);
-
-          if (fragElement !== null) {
-            /*
-            Variables:
-            - 0.81: Minimal scroll duration
-            - 2000: If smaller, scroll duration is longer; If bigger, scroll duration is shorter
-            - Power3.easeOut: Easing function from GSAP library
-            */
-            gsap.to(window, {
-              duration: Math.max(0.81, Math.log((fragElement.offsetTop - window.scrollY) / 2000)),
-              scrollTo: id,
-              ease: Power3.easeOut
-            });
+            if (fragElement !== null) {
+              /*
+              Variables:
+              - 0.81: Minimal scroll duration
+              - 2000: If smaller, scroll duration is longer; If bigger, scroll duration is shorter
+              - Power3.easeOut: Easing function from GSAP library
+              */
+              gsap.to(window, {
+                duration: Math.max(0.81, Math.log((fragElement.offsetTop - window.scrollY) / 2000)),
+                scrollTo: id,
+                ease: Power3.easeOut
+              });
+            }
           }
         }
       });
